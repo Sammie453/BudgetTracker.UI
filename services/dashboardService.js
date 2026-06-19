30973cc7b27564a990a45b3dfc438ae297eb9a92
Sammie453 <aphiwembelu992@gmail.com>
2026-06-19 09:19:34 +0200

    fixed all issues

--- services/dashboardService.js
+++ services/dashboardService.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getDashboardData = async () => {
-  const response =
-    await axios.get(`${API_URL}/dashboard`);
-
+  const response = await axios.get(`${API_URL}/dashboard`);
   return response.data;
 };
